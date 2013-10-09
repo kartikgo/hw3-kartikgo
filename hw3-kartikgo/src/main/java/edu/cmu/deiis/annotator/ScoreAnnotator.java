dd59e45f2849c6f61440ff8c559dae156d8e9f2e
kartik <kartik.g.2789@gmail.com>
2013-10-08 21:06:27 -0400

    Do comparisons and implement some scoring changes

--- hw3-kartikgo/src/main/java/edu/cmu/deiis/annotator/ScoreAnnotator.java
+++ hw3-kartikgo/src/main/java/edu/cmu/deiis/annotator/ScoreAnnotator.java
@@ -18,155 +18,203 @@ import org.apache.uima.cas.FSIndex;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.resource.ResourceInitializationException;
+import org.cleartk.ne.type.NamedEntityMention;
 
 import edu.cmu.deiis.types.*;
+
 /**
- * This calculates overlaps of tokens, ngrams, part of speech tags(both tokens and ngrams), lemma(bith tokens and ngrams) 
- * Finally a score which is a weighted sum of these attributes is calculated.
+ * This calculates overlaps of tokens, ngrams, part of speech tags(both tokens and ngrams),
+ * lemma(bith tokens and ngrams) Finally a score which is a weighted sum of these attributes is
+ * calculated.
+ * 
  * @author Kartik Goyal
- *
+ * 
  */
 public class ScoreAnnotator extends JCasAnnotator_ImplBase {
-  
+
   public void process(JCas jCas) throws AnalysisEngineProcessException {
 
     FSIndex quesIdx = jCas.getAnnotationIndex(Question.type);
     FSIndex ansIdx = jCas.getAnnotationIndex(Answer.type);
-    FSIndex tokenIdx= jCas.getAnnotationIndex(Token.type);
-    FSIndex ngIdx= jCas.getAnnotationIndex(NGram.type);
+    FSIndex tokenIdx = jCas.getAnnotationIndex(Token.type);
+    FSIndex ngIdx = jCas.getAnnotationIndex(NGram.type);
+    FSIndex neIdx = jCas.getAnnotationIndex(NamedEntityMention.type);
     Iterator que = quesIdx.iterator();
     Iterator ans = ansIdx.iterator();
-    Iterator tok= tokenIdx.iterator();
-    Iterator ng= ngIdx.iterator();
-    ArrayList<Token> questionTok= new ArrayList<Token>();
-    ArrayList<NGram> questionGram= new ArrayList<NGram>();
-    while(que.hasNext()){
-      Question q=(Question)que.next();
+    Iterator tok = tokenIdx.iterator();
+    Iterator ng = ngIdx.iterator();
+    Iterator ne = neIdx.iterator();
+    ArrayList<Token> questionTok = new ArrayList<Token>();
+    ArrayList<NGram> questionGram = new ArrayList<NGram>();
+    ArrayList<NamedEntityMention> questionne = new ArrayList<NamedEntityMention>();
+    while (que.hasNext()) {
+      Question q = (Question) que.next();
       int qbeg = q.getBegin();
       int qend = q.getEnd();
-      while (tok.hasNext()){
-        Token t= (Token)tok.next();
-        if ((t.getBegin()>=qbeg)&&(t.getEnd()<=qend)){
+      while (tok.hasNext()) {
+        Token t = (Token) tok.next();
+        if ((t.getBegin() >= qbeg) && (t.getEnd() <= qend)) {
           questionTok.add(t);
         }
       }
-      while(ng.hasNext()){
-        NGram n= (NGram)ng.next();
-        if ((n.getBegin()>=qbeg)&&(n.getEnd()<=qend)){
+      while (ng.hasNext()) {
+        NGram n = (NGram) ng.next();
+        if ((n.getBegin() >= qbeg) && (n.getEnd() <= qend)) {
           questionGram.add(n);
         }
       }
+      while (ne.hasNext()) {
+        NamedEntityMention nen = (NamedEntityMention) ne.next();
+        if ((nen.getBegin() >= qbeg) && (nen.getEnd() <= qend)) {
+          questionne.add(nen);
+        }
+      }
     }
-    
-    while(ans.hasNext()){
-      tok= tokenIdx.iterator();
-      ng= ngIdx.iterator();
-      ArrayList<Token> answerTok= new ArrayList<Token>();
-      ArrayList<NGram> answerGram= new ArrayList<NGram>();
-      Answer a=(Answer)ans.next();
+
+    while (ans.hasNext()) {
+      tok = tokenIdx.iterator();
+      ng = ngIdx.iterator();
+      ne = neIdx.iterator();
+      ArrayList<Token> answerTok = new ArrayList<Token>();
+      ArrayList<NGram> answerGram = new ArrayList<NGram>();
+      ArrayList<NamedEntityMention> answerne = new ArrayList<NamedEntityMention>();
+      Answer a = (Answer) ans.next();
       int abeg = a.getBegin();
       int aend = a.getEnd();
-      while (tok.hasNext()){
-        Token t= (Token)tok.next();
-        if ((t.getBegin()>=abeg)&&(t.getEnd()<=aend)){
+      while (tok.hasNext()) {
+        Token t = (Token) tok.next();
+        if ((t.getBegin() >= abeg) && (t.getEnd() <= aend)) {
           answerTok.add(t);
         }
       }
-      
-      
-      double totTok = (double)answerTok.size();
-      double tokOl=0;
-      double POSOl=0;
-      double lemmaOl=0;
-      for(Token qtok: questionTok){
-        for (Token atok:answerTok){
-          if (atok.getCoveredText().equals(qtok.getCoveredText())){
-            tokOl +=1;
+
+      double totTok = (double) answerTok.size();
+      double tokOl = 0;
+      double POSOl = 0;
+      double lemmaOl = 0;
+      for (Token qtok : questionTok) {
+        for (Token atok : answerTok) {
+          if (atok.getCoveredText().equals(qtok.getCoveredText())) {
+            tokOl += 1;
           }
-          if (atok.getPOS().equals(qtok.getPOS())){
-            POSOl +=1;
+          if (atok.getPOS().equals(qtok.getPOS())) {
+            POSOl += 1;
           }
-          if (atok.getPOS().equals(qtok.getPOS())){
-            lemmaOl +=1;
+          if (atok.getPOS().equals(qtok.getPOS())) {
+            lemmaOl += 1;
           }
         }
       }
-      double tokScore= tokOl/totTok;
-      double POSScore= POSOl/totTok;
-      double lemmaScore= lemmaOl/totTok;
-      
-      
-      while (ng.hasNext()){
-        NGram n= (NGram)ng.next();
-        if ((n.getBegin()>=abeg)&&(n.getEnd()<=aend)){
+      double tokScore = tokOl / totTok;
+      double POSScore = POSOl / totTok;
+      double lemmaScore = lemmaOl / totTok;
+
+      while (ne.hasNext()) {
+        NamedEntityMention t = (NamedEntityMention) ne.next();
+        if ((t.getBegin() >= abeg) && (t.getEnd() <= aend)) {
+          answerne.add(t);
+        }
+      }
+
+      double totne = (double) answerne.size();
+      double neOl = 0;
+
+      for (NamedEntityMention qne : questionne) {
+        for (NamedEntityMention ane : answerne) {
+          if (ane.getCoveredText().equals(qne.getCoveredText())) {
+            neOl += 1;
+          }
+
+        }
+      }
+      double neScore = 0.0;
+      if (totne != 0) {
+        neScore = neOl / totne;
+      } else {
+        neScore = 0.0;
+      }
+
+      while (ng.hasNext()) {
+        NGram n = (NGram) ng.next();
+        if ((n.getBegin() >= abeg) && (n.getEnd() <= aend)) {
           answerGram.add(n);
         }
       }
-      
-      double totgram = (double)answerGram.size();
-      double gramOl=0;
-      double gramPOSOl=0;
-      double gramlemmaOl=0;
-      for(NGram qn: questionGram){
-        for (NGram an:answerGram){
-          if(an.getElements().size()==qn.getElements().size()){
+
+      double totgram = (double) answerGram.size();
+      double gramOl = 0;
+      double gramPOSOl = 0;
+      double gramlemmaOl = 0;
+      for (NGram qn : questionGram) {
+        for (NGram an : answerGram) {
+          if (an.getElements().size() == qn.getElements().size()) {
             FSArray aElements = an.getElements();
             FSArray qElements = qn.getElements();
-            if(an.getElements().size()==1){
-              Token aFirst = (Token)aElements.get(0);
-              Token qFirst= (Token)qElements.get(0);
-              if(aFirst.getCoveredText().equals(qFirst.getCoveredText())){
-                gramOl +=1;
+            if (an.getElements().size() == 1) {
+              Token aFirst = (Token) aElements.get(0);
+              Token qFirst = (Token) qElements.get(0);
+              if (aFirst.getCoveredText().equals(qFirst.getCoveredText())) {
+                gramOl += 1;
               }
-              if(aFirst.getLemma().equals(qFirst.getLemma())){
-                gramlemmaOl +=1;
+              if (aFirst.getLemma().equals(qFirst.getLemma())) {
+                gramlemmaOl += 1;
               }
-              if(aFirst.getPOS().equals(qFirst.getPOS())){
-                gramPOSOl +=1;
-              }
-            }
-            if(an.getElements().size()==2){
-              Token aFirst = (Token)aElements.get(0);
-              Token qFirst= (Token)qElements.get(0);
-              Token aSecond = (Token)aElements.get(1);
-              Token qSecond= (Token)qElements.get(1);
-              if(aFirst.getCoveredText().equals(qFirst.getCoveredText())&&(aSecond.getCoveredText().equals(qSecond.getCoveredText()))){
-                gramOl +=1;
-              }
-              if(aFirst.getLemma().equals(qFirst.getLemma())&&(aSecond.getLemma().equals(qSecond.getLemma()))){
-                gramlemmaOl +=1;
-              }
-              if(aFirst.getPOS().equals(qFirst.getPOS())&&(aSecond.getPOS().equals(qSecond.getPOS()))){
-                gramPOSOl +=1;
+              if (aFirst.getPOS().equals(qFirst.getPOS())) {
+                gramPOSOl += 1;
               }
             }
-            if(an.getElements().size()==3){
-              Token aFirst = (Token)aElements.get(0);
-              Token qFirst= (Token)qElements.get(0);
-              Token aSecond = (Token)aElements.get(1);
-              Token qSecond= (Token)qElements.get(1);
-              Token aThird = (Token)aElements.get(2);
-              Token qThird= (Token)qElements.get(2);
-              if(aFirst.getCoveredText().equals(qFirst.getCoveredText())&&(aSecond.getCoveredText().equals(qSecond.getCoveredText()))&&(aThird.getCoveredText().equals(qThird.getCoveredText()))){
-                gramOl +=1;
+            if (an.getElements().size() == 2) {
+              Token aFirst = (Token) aElements.get(0);
+              Token qFirst = (Token) qElements.get(0);
+              Token aSecond = (Token) aElements.get(1);
+              Token qSecond = (Token) qElements.get(1);
+              if (aFirst.getCoveredText().equals(qFirst.getCoveredText())
+                      && (aSecond.getCoveredText().equals(qSecond.getCoveredText()))) {
+                gramOl += 1;
               }
-              if(aFirst.getLemma().equals(qFirst.getLemma())&&(aSecond.getLemma().equals(qSecond.getLemma()))&&(aThird.getLemma().equals(qThird.getLemma()))){
-                gramlemmaOl +=1;
+              if (aFirst.getLemma().equals(qFirst.getLemma())
+                      && (aSecond.getLemma().equals(qSecond.getLemma()))) {
+                gramlemmaOl += 1;
               }
-              if(aFirst.getPOS().equals(qFirst.getPOS())&&(aSecond.getPOS().equals(qSecond.getPOS()))&&(aThird.getPOS().equals(qThird.getPOS()))){
-                gramPOSOl +=1;
+              if (aFirst.getPOS().equals(qFirst.getPOS())
+                      && (aSecond.getPOS().equals(qSecond.getPOS()))) {
+                gramPOSOl += 1;
+              }
+            }
+            if (an.getElements().size() == 3) {
+              Token aFirst = (Token) aElements.get(0);
+              Token qFirst = (Token) qElements.get(0);
+              Token aSecond = (Token) aElements.get(1);
+              Token qSecond = (Token) qElements.get(1);
+              Token aThird = (Token) aElements.get(2);
+              Token qThird = (Token) qElements.get(2);
+              if (aFirst.getCoveredText().equals(qFirst.getCoveredText())
+                      && (aSecond.getCoveredText().equals(qSecond.getCoveredText()))
+                      && (aThird.getCoveredText().equals(qThird.getCoveredText()))) {
+                gramOl += 1;
+              }
+              if (aFirst.getLemma().equals(qFirst.getLemma())
+                      && (aSecond.getLemma().equals(qSecond.getLemma()))
+                      && (aThird.getLemma().equals(qThird.getLemma()))) {
+                gramlemmaOl += 1;
+              }
+              if (aFirst.getPOS().equals(qFirst.getPOS())
+                      && (aSecond.getPOS().equals(qSecond.getPOS()))
+                      && (aThird.getPOS().equals(qThird.getPOS()))) {
+                gramPOSOl += 1;
               }
             }
           }
-          
+
         }
       }
-      double gramScore= gramOl/totgram;
-      double gramPOSScore= gramPOSOl/totgram;
-      double gramlemmaScore= gramlemmaOl/totgram;
-      //Setting answerscore
-      double combinedScore=((0.3*tokScore)+(0.15*lemmaScore)+(0.15*gramlemmaScore)+(0.05*POSScore)+(0.05*gramPOSScore)+(0.3*gramScore));
-      AnswerScore tokLevelScore= new AnswerScore(jCas);
+      double gramScore = gramOl / totgram;
+      double gramPOSScore = gramPOSOl / totgram;
+      double gramlemmaScore = gramlemmaOl / totgram;
+      // Setting answerscore
+      double combinedScore = ((0.25 * tokScore) + (0.25 * gramScore) + (0.05 * neScore)
+              + (0.20 * lemmaScore) + (0.15 * gramlemmaScore) + (0.05 * POSScore) + (0.05 * gramPOSScore));
+      AnswerScore tokLevelScore = new AnswerScore(jCas);
       tokLevelScore.setAnswer(a);
       tokLevelScore.setScore(combinedScore);
       tokLevelScore.setCasProcessorId("Token-NGram-Score");
